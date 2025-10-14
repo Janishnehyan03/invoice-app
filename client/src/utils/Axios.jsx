23157517912a116919bf7ff732fbd97e9eb483eb
Janishnehyan03 <nehyanjanish@gmail.com>
2025-10-14 12:12:16 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5100/api",
-  // baseURL: "/api",
+  // baseURL: process.env.REACT_APP_API_URL || "http://localhost:5100/api",
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
